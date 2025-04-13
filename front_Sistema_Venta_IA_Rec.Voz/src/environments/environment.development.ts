eb82ae4fd008fcbd71b8785c613affbf895a4fdc
Fournext <sebastianzc04@gmail.com>
2025-04-13 13:32:20 -0400

    Endpoint local y publico del back

--- front_Sistema_Venta_IA_Rec.Voz/src/environments/environment.development.ts
+++ front_Sistema_Venta_IA_Rec.Voz/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    endpoint: 'http://127.0.0.1:8000/'
+    endpoint: 'http://ec2-3-144-124-91.us-east-2.compute.amazonaws.com:8000/',
+    enpointlocal: 'http://localhost:8000/'
 };
