5bb9ac0ad2c650258b1f415cb42b246c0e9a2822
Fournext <sebastianzc04@gmail.com>
2025-04-13 15:30:39 -0400

    endpoint 2

--- front_Sistema_Venta_IA_Rec.Voz/src/environments/environment.development.ts
+++ front_Sistema_Venta_IA_Rec.Voz/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    endpoint: 'http://ec2-3-144-124-91.us-east-2.compute.amazonaws.com:8000/',
+    endpoint: 'http://ec2-18-224-51-43.us-east-2.compute.amazonaws.com:8000/',
     enpointlocal: 'http://localhost:8000/'
 };
